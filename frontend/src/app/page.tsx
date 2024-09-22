35b1f179518ebff80745e87655508752113653bb
parikshitadhikari <parikshitadhikari@gmail.com>
2024-09-22 18:43:36 +0545

    landing page components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,47 +1,29 @@
 import { BackgroundBeams } from "@/components/ui/background-beams";
-import { TypewriterEffect } from "@/components/ui/typewriter-effect";
+import Box from "./components/Box";
+import Feature from "./components/Feature";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+import Service from "./components/Service";
+import Testimonial from "./components/Testimonial";
 
 export default function Home() {
-  const words = [
-    {
-      text: "Build",
-    },
-    {
-      text: "awesome",
-    },
-    {
-      text: "apps",
-    },
-    {
-      text: "with",
-    },
-    {
-      text: "Aceternity.",
-      className: "text-blue-500 dark:text-blue-500",
-    },
-  ];
   return (
     <div>
       <div className="">
         <BackgroundBeams />
       </div>
-      <div className="flex flex-col items-center justify-center h-[40rem] ">
-      <p className="text-neutral-600 dark:text-neutral-200 text-base  mb-10">
-        The road to freedom starts from here
-      </p>
-      <TypewriterEffect words={words} />
-      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
-        <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
-          Join now
-        </button>
-        <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
-          Signup
-        </button>
+      <div className="z-50">
+        <div>
+          <NavBar />
+          <Hero />
+          <Box />
+          <Service />
+          <Feature />
+          <Testimonial />
+          <Footer />
+        </div>
       </div>
-    </div>
     </div>
   );
 }
-
-
-
