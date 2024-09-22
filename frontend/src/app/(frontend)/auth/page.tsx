4ec192f4b51f56d879f68041cf83d20e63836a99
parikshitadhikari <parikshitadhikari@gmail.com>
2024-09-22 20:12:48 +0545

    auth page forms

--- frontend/src/app/(frontend)/auth/page.tsx
+++ frontend/src/app/(frontend)/auth/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+import NavBar from "@/app/components/NavBar";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 import React from "react";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  return (
+    <div className="relative">
+      <div className="absolute inset-0 z-0">
+        <BackgroundBeams />
+      </div>
+      <div>
+        <NavBar />
+      </div>
+      <Login />
+      <SignUp />
+    </div>
+  );
 };
 
 export default Auth;
