d666552a8f6ad7b41c5cbc60f1cbf5c82766db20
Rohan Acharya <rohanacharya38@gmail.com>
2024-09-22 22:22:25 +0545

    added post view to url

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TravellersViewSet,EventViewSet
+from .views import TravellersViewSet,EventViewSet,PostViewSet
 
 router = DefaultRouter()
 router.register(r'travellers', TravellersViewSet)
 router.register(r'events',EventViewSet)
+router.register(r'posts',PostViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
