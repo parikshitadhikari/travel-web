2bbb6012f795553d0667e30d623fd48397846048
parikshitadhikari <parikshitadhikari@gmail.com>
2024-09-23 17:38:01 +0545

    card in recommended

--- frontend/src/app/(frontend)/itenaries/components/Recommended.tsx
+++ frontend/src/app/(frontend)/itenaries/components/Recommended.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import ItenariesCard from "./ItenariesCard";
+import "@mantine/core/styles.css";
 
 const Recommended = () => {
   return (
-    <div>Recommended</div>
-  )
-}
+    <div className="px-20">
+      <h1>RECOMMENDED FOR YOU</h1>
+      <div className="grid grid-cols-3 gap-x-16">
+        <ItenariesCard />
+      </div>
+    </div>
+  );
+};
 
-export default Recommended
+export default Recommended;
--- frontend/src/app/(frontend)/itenaries/page.tsx
+++ frontend/src/app/(frontend)/itenaries/page.tsx
@@ -1,26 +1,29 @@
 import NavBar from "@/app/components/NavBar";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 import React from "react";
-import ItenariesCard from "./components/ItenariesCard";
 import Recommended from "./components/Recommended";
 import Trending from "./components/Trending";
 import LowBudget from "./components/LowBudget";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 
 const Itenaries = () => {
   return (
-    <div className="relative min-h-screen bg-white">
-      <div className="absolute inset-0 z-0">
-        <BackgroundBeams />
-      </div>
+    <MantineProvider>
+      <div className="relative min-h-screen bg-white">
+        <div className="absolute inset-0 z-0">
+          <BackgroundBeams />
+        </div>
 
-      <NavBar />
+        <NavBar />
 
-      <div className="flex flex-col items-center justify-center mt-10 space-y-4 z-10 relative">
-        <Recommended />
-        <Trending />
-        <LowBudget />
+        <div className="flex flex-col items-center justify-center mt-10 space-y-4 z-10 relative">
+          <Recommended />
+          <Trending />
+          <LowBudget />
+        </div>
       </div>
-    </div>
+    </MantineProvider>
   );
 };
 
