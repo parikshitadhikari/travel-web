2597010552a5051cc9f7b645e6e18fdccd3f7bff
Ujjwal Jha <ujjwaljha1225@gmail.com>
2024-09-23 09:53:24 +0545

    event page

--- frontend/src/app/(frontend)/event/page.tsx
+++ frontend/src/app/(frontend)/event/page.tsx
@@ -1,9 +1,11 @@
+import SidebarDemo from '@/app/components/Sidebar'
 import ExpandableCardDemo from '@/components/blocks/expandable-card-demo-grid'
 import React from 'react'
 
 const Event = () => {
   return (
     <div>
+      <SidebarDemo/>
       <ExpandableCardDemo/>
     </div>
   )
