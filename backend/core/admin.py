a781f9f15742026c9b9e8c4bf816cef5c3080cee
Rohan Acharya <rohanacharya38@gmail.com>
2024-09-24 13:14:31 +0545

    Added reviews sentiment percentage

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -99,97 +99,98 @@ class PackageAdmin(admin.ModelAdmin):
     list_display = ("id", "name",  "price", )
     search_fields = ("name", )
     filter_horizontal = ("label",)
-    # readonly_fields= ['get_reviews']
-    # def get_reviews(self,obj):
+    readonly_fields= ['reviews_percentage']
+    def reviews_percentage(self,obj):
         
 
-    #     comments = PackageComment.objects.filter(package__id=obj.id).values_list('comment',flat=True)
-    #     print(comments)
-    #     print(len(comments))
-    #     if(len(comments)==0):
-    #         return "no reviews"
-    #     from tf_keras.models import load_model
-    #     from tf_keras.preprocessing.sequence import pad_sequences
-    #     from tf_keras.preprocessing.text import Tokenizer
-    #     import numpy as np
-    #     import json
-    #     import pandas as pd
-    #     from pathlib import Path
+        comments = PackageComment.objects.filter(package__id=obj.id).values_list('comment',flat=True)
+        print(comments)
+        print(len(comments))
+        if(len(comments)==0):
+            return "no reviews"
+        from tf_keras.models import load_model
+        from tf_keras.preprocessing.sequence import pad_sequences
+        from tf_keras.preprocessing.text import Tokenizer
+        import numpy as np
+        import json
+        import pandas as pd
+        from pathlib import Path
 
-    #     try:
-    #         # Load Model
-    #         DIR = Path(__file__).resolve().parent.parent
-    #         print(DIR)
-    #         model_path =  DIR / "core" /"machineLearning" / "model.h5"
-    #         # model_path =  "/core/machineLearning/model.h5"
-    #         model = load_model(model_path)
+        try:
+            # Load Model
+            DIR = Path(__file__).resolve().parent.parent
+            print(DIR)
+            model_path =  DIR / "core" /"machineLearning" / "model.h5"
+            # model_path =  "/core/machineLearning/model.h5"
+            model = load_model(model_path)
 
-    #         print(model_path)
+            print(model_path)
 
-    #         # CSV path
-    #         csv_file =DIR / "core" /"machineLearning" / "output.txt"
+            # CSV path
+            csv_file =DIR / "core" /"machineLearning" / "output.txt"
 
-    #         # Reading from CSV file
-    #         with open(csv_file, mode="r") as file:
-    #             data = [line.strip() for line in file]
+            # Reading from CSV file
+            with open(csv_file, mode="r") as file:
+                data = [line.strip() for line in file]
 
-    #         max_words = 5000
-    #         max_len = 600
+            max_words = 5000
+            max_len = 600
 
-    #         tokenizer = Tokenizer(num_words=max_words)
-    #         tokenizer.fit_on_texts(data)
-
-
-    #         def predictions(df):
-    #             reviewText = df["reviewText"].tolist()
-    #             num = df.shape[0]
-    #             sequence = tokenizer.texts_to_sequences(reviewText)
-    #             test_review = pad_sequences(sequence, maxlen=max_len)
-
-    #             sentiment = []
-    #             print(df)
-    #             for i in range(num):
-    #                 eachsentiment = ["Negative", "Neutral", "Positive"][
-    #                     np.around(model.predict(test_review), decimals=4).argmax(axis=1)[i]
-    #                 ]
-    #                 sentiment.append(eachsentiment)
-
-    #             positiveCount = sentiment.count("Positive")
-    #             negativeCount = sentiment.count("Negative")
-    #             neutralCount = sentiment.count("Neutral")
-
-    #             positivePercentage = positiveCount / num
-    #             negativePercentage = negativeCount / num
-    #             neutralPercentage = neutralCount / num
+            tokenizer = Tokenizer(num_words=max_words)
+            tokenizer.fit_on_texts(data)
 
 
-    #             resultDataDic = {"comments": {}}
-    #             for i, text in enumerate(reviewText):
-    #                 resultDataDic["comments"][text] = sentiment[i]
+            def predictions(df):
+                reviewText = df["reviewText"].tolist()
+                num = df.shape[0]
+                sequence = tokenizer.texts_to_sequences(reviewText)
+                test_review = pad_sequences(sequence, maxlen=max_len)
 
-    #             resultDataDic["Percentage"] = {
-    #                 "Positive": positivePercentage,
-    #                 "Negative": negativePercentage,
-    #                 "Neutral": neutralPercentage,
-    #             }
-    #             return resultDataDic
-    #             # with open(BASE_DIR / "machineLearning" / "output.json", "w") as outfile:
-    #             #     json.dump(resultDataDic, outfile)
+                sentiment = []
+                print(df)
+                for i in range(num):
+                    eachsentiment = ["Negative", "Neutral", "Positive"][
+                        np.around(model.predict(test_review), decimals=4).argmax(axis=1)[i]
+                    ]
+                    sentiment.append(eachsentiment)
 
-    #     except Exception as e:
-    #         print(f"An error occurred: {e}")
+                positiveCount = sentiment.count("Positive")
+                negativeCount = sentiment.count("Negative")
+                neutralCount = sentiment.count("Neutral")
 
-    #     # Get the current time
-    #     current_time = timezone.now()
+                positivePercentage = positiveCount / num
+                negativePercentage = negativeCount / num
+                neutralPercentage = neutralCount / num
 
-    #     # Create a list of dictionaries for DataFrame
-    #     data = [{'reviewText': comment, 'reviewTime': current_time} for comment in comments]
-    #     if(data.count==0):
-    #         return "No reviews"
+
+                resultDataDic = {"comments": {}}
+                for i, text in enumerate(reviewText):
+                    resultDataDic["comments"][text] = sentiment[i]
+
+                resultDataDic["Percentage"] = {
+                    "Positive": positivePercentage,
+                    "Negative": negativePercentage,
+                    "Neutral": neutralPercentage,
+                }
+                return resultDataDic
+                # with open(BASE_DIR / "machineLearning" / "output.json", "w") as outfile:
+                #     json.dump(resultDataDic, outfile)
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+        # Get the current time
+        current_time = timezone.now()
+
+        # Create a list of dictionaries for DataFrame
+        data = [{'reviewText': comment, 'reviewTime': current_time} for comment in comments]
+        if(data.count==0):
+            return "No reviews"
             
-    #     print(data)
-    #     # Create a pandas DataFrame from the data
-    #     df = pd.DataFrame(data)
+        print(data)
+        # Create a pandas DataFrame from the data
+        df = pd.DataFrame(data)
 
-    #     json_result = predictions(df=df)
-    #     return json_result
+        json_result = predictions(df=df)
+        percentage= json_result.get('Percentage',json_result)
+        return f"Positive : {percentage.get('Positive')}\n Negative : {percentage.get('Negative')} \n Neutral : {percentage.get('Neutral')}";
