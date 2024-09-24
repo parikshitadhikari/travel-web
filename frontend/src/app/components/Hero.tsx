b26657a4bd848b183f7c208cbbd596bfc86c2e14
Sudeep <i.sudeep.subedi@gmail.com>
2024-09-24 08:39:46 +0545

    Changed auth page

--- frontend/src/app/components/Hero.tsx
+++ frontend/src/app/components/Hero.tsx
@@ -57,9 +57,11 @@ const Hero = () => {
           </div>
           */}
           <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-            <button className="w-40 h-10 rounded-xl bg-slate-600 border dark:border-white border-transparent text-white text-sm">
-              Join now
-            </button>
+            <Link href={'/auth'}>
+              <button className="w-40 h-10 rounded-xl bg-slate-600 border dark:border-white border-transparent text-white text-sm">
+                Join now
+              </button>
+            </Link>
             {/* <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
               Signup
             </button> */}
