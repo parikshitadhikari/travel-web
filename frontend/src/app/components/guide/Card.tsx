9cbe9a9f4befebdf9c8f507684d9f2faa46b8c9c
Sudeep <i.sudeep.subedi@gmail.com>
2024-09-24 12:00:12 +0545

    Guide page content.

--- frontend/src/app/components/guide/Card.tsx
+++ frontend/src/app/components/guide/Card.tsx
@@ -3,6 +3,10 @@ import Image from "next/image";
 import React, { useEffect, useId, useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useOutsideClick } from "@/app/hooks/use-outside-click";
+import Ujjwal from "@/images/guides/ujjwal.jpg"
+import Guide1 from "@/images/guides/guide1.jpeg"
+import Guide2 from "@/images/guides/guide2.jpeg"
+import Sardul from "@/images/guides/sardul.jpg"
 
 export default function ExpandableCardDemo() {
   const [active, setActive] = useState<(typeof cards)[number] | boolean | null>(
@@ -203,91 +207,74 @@ export const CloseIcon = () => {
 
 const cards = [
   {
-    description: "Lana Del Rey",
-    title: "Summertime Sadness",
-    src: "https://assets.aceternity.com/demos/lana-del-rey.jpeg",
+    description: "Arjun Dhara",
+    title: "Ujjwal Jha",
+    src: Ujjwal,
     ctaText: "Book",
-    ctaLink: "https://ui.aceternity.com/templates",
+    ctaLink: "#",
     content: () => {
       return (
         <p>
-          Lana Del Rey, an iconic American singer-songwriter, is celebrated for
-          her melancholic and cinematic music style. Born Elizabeth Woolridge
-          Grant in New York City, she has captivated audiences worldwide with
-          her haunting voice and introspective lyrics. <br /> <br /> Her songs
-          often explore themes of tragic romance, glamour, and melancholia,
-          drawing inspiration from both contemporary and vintage pop culture.
-          With a career that has seen numerous critically acclaimed albums, Lana
-          Del Rey has established herself as a unique and influential figure in
-          the music industry, earning a dedicated fan base and numerous
-          accolades.
+          Sophie is all about immersing herself in the local culture.<br />
+          With a deep love for art, history, and authentic experiences, 
+          she’ll guide you to the hidden gems of each city, 
+          from quirky museums to vibrant street festivals. If you’re 
+          looking to explore the soul of a destination, Sophie’s got you covered
         </p>
       );
     },
   },
   {
-    description: "Babbu Maan",
-    title: "Mitran Di Chhatri",
-    src: "https://assets.aceternity.com/demos/babbu-maan.jpeg",
+    description: "Jheel",
+    title: "Ravi Tiwari",
+    src: Guide1,
     ctaText: "Book",
-    ctaLink: "https://ui.aceternity.com/templates",
+    ctaLink: "#",
     content: () => {
       return (
         <p>
-          Babu Maan, a legendary Punjabi singer, is renowned for his soulful
-          voice and profound lyrics that resonate deeply with his audience. Born
-          in the village of Khant Maanpur in Punjab, India, he has become a
-          cultural icon in the Punjabi music industry. <br /> <br /> His songs
-          often reflect the struggles and triumphs of everyday life, capturing
-          the essence of Punjabi culture and traditions. With a career spanning
-          over two decades, Babu Maan has released numerous hit albums and
-          singles that have garnered him a massive fan following both in India
-          and abroad.
+           Sophie is all about immersing herself in the local culture.<br />
+          With a deep love for art, history, and authentic experiences, 
+          she’ll guide you to the hidden gems of each city, 
+          from quirky museums to vibrant street festivals. If you’re 
+          looking to explore the soul of a destination, Sophie’s got you covered.
         </p>
       );
     },
   },
 
   {
-    description: "Metallica",
-    title: "For Whom The Bell Tolls",
-    src: "https://assets.aceternity.com/demos/metallica.jpeg",
+    description: "Barpak",
+    title: "Shiela Johns",
+    src: Guide2,
     ctaText: "Book",
-    ctaLink: "https://ui.aceternity.com/templates",
+    ctaLink: "#",
     content: () => {
       return (
         <p>
-          Metallica, an iconic American heavy metal band, is renowned for their
-          powerful sound and intense performances that resonate deeply with
-          their audience. Formed in Los Angeles, California, they have become a
-          cultural icon in the heavy metal music industry. <br /> <br /> Their
-          songs often reflect themes of aggression, social issues, and personal
-          struggles, capturing the essence of the heavy metal genre. With a
-          career spanning over four decades, Metallica has released numerous hit
-          albums and singles that have garnered them a massive fan following
-          both in the United States and abroad.
+          Sophie is all about immersing herself in the local culture.<br />
+          With a deep love for art, history, and authentic experiences, 
+          she’ll guide you to the hidden gems of each city, 
+          from quirky museums to vibrant street festivals. If you’re 
+          looking to explore the soul of a destination, Sophie’s got you covered.
         </p>
       );
     },
   },
   {
-    description: "Lord Himesh",
-    title: "Aap Ka Suroor",
-    src: "https://assets.aceternity.com/demos/aap-ka-suroor.jpeg",
+    description: "Markhu",
+    title: "Sardul Chamrakar",
+    src: Sardul,
     ctaText: "Book",
-    ctaLink: "https://ui.aceternity.com/templates",
+    ctaLink: "#",
     content: () => {
       return (
         <p>
-          Himesh Reshammiya, a renowned Indian music composer, singer, and
-          actor, is celebrated for his distinctive voice and innovative
-          compositions. Born in Mumbai, India, he has become a prominent figure
-          in the Bollywood music industry. <br /> <br /> His songs often feature
-          a blend of contemporary and traditional Indian music, capturing the
-          essence of modern Bollywood soundtracks. With a career spanning over
-          two decades, Himesh Reshammiya has released numerous hit albums and
-          singles that have garnered him a massive fan following both in India
-          and abroad.
+          Sophie is all about immersing herself in the local culture.<br />
+          With a deep love for art, history, and authentic experiences, 
+          she’ll guide you to the hidden gems of each city, 
+          from quirky museums to vibrant street festivals. If you’re 
+          looking to explore the soul of a destination, Sophie’s got you covered. 
         </p>
       );
     },
